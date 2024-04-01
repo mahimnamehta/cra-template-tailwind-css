f272a51008a5a5fe865b4c67baaa97d0bc06d126
mahimnamehta <65457672+mahimnamehta@users.noreply.github.com>
2024-04-01 11:46:45 -0400

    Update initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# cra-template-tailwindcss
+
+npx create-react-app --template cra-template-tailwind
---
+++ template/src/App.js
@@ -0,0 +1,10 @@
+import './generated.css'
+function App() {
+  return (
+    <div className="App">
+      TailwindCSS App
+    </div>
+  );
+}
+
+export default App;
